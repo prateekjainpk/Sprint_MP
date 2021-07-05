1f69a11a209c5921d13bab0adc85c08c3bb0f4ad
shubhamsfs <shubhamsfssharma@gmail.com>
2021-07-05 11:45:41 +0530

    pushed

---
+++ src/main/java/demo/Sprint/Trial.java
@@ -0,0 +1,10 @@
+package demo.Sprint;
+
+import org.openqa.selenium.WebDriver;
+
+public class Trial {
+    public static void main(String[] args) {
+        WebDriver driver;
+        System.out.println("printed");
+    }
+}
